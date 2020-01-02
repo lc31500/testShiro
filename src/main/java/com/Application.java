ea4fd29dda4659c06c3ee89631d891c39403d7ab
tom_glb <tom_glb@haima.me>
2020-01-02 19:08:19 +0800

    1.0

---
+++ src/main/java/com/Application.java
@@ -0,0 +1,14 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    /**
+     * SpringBoot启动类
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
